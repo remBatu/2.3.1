1cc22e086fda8310c406ada9d6ed00c548185c78
Владимир Хубецов <shepgrade@gmail.com>
2026-05-15 20:33:02 +0300

    Практическая задача 2.3.1, мвц часть

--- src/main/java/web/controller/StartPageController.java
+++ src/main/java/web/controller/StartPageController.java
@@ -1,14 +1,13 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StartPageController {
 
 	@GetMapping(value = "/")
-	public String printWelcome() {
+	public String getStartPage() {
 		return "index";
 	}
 	
